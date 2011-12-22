cb31cd9490a6c898a7bc85c1fecf531c1213b884
Johan Brook <johan.jvb@gmail.com>
2011-12-22 14:52:37 +0100

    Adjustments to Compass config

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@
 
 
 # Set this to the root of your project (usually where this file lives) when deployed:
-http_path = (environment == :production) ? "/compass-boilerplate"  : "/sub-folder/compass-boilerplate"
+# http_path = "/"
 
 css_dir = "stylesheets"
 sass_dir = "sass"
@@ -16,7 +16,7 @@ javascripts_dir = "javascripts"
 output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
